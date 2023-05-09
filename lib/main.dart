118e9e4ae324af91d61db8946109ce75e9086b43
LiamWhitenack <liamwhitenack@outlook.com>
2023-05-09 13:15:27 -0400

    small fix

--- lib/main.dart
+++ lib/main.dart
@@ -25,10 +25,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    int numberOfKingdoms = 7;
+    int numberOfKingdoms = 2;
     int numberOfRounds = 13;
     List<Kingdom> kingdoms = generateListOfNKingdoms(numberOfKingdoms);
-    if (numberOfKingdoms == 2) numberOfRounds = 2;
+    if (numberOfKingdoms == 2) numberOfRounds = 7;
     return MaterialApp(
       title: 'Kingdomino',
       home: Scaffold(
